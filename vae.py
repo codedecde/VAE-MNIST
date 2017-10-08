3afce8ac37890ac48f7aef554b7134506b42465e
codedecde <barunpatra95@gmail.com>
2017-10-08 00:14:47 -0400

    Fixed VAE

--- vae.py
+++ vae.py
@@ -1,5 +1,5 @@
 import scipy.io as sio
-from random import shuffle, random, choice
+from random import shuffle, choice
 import torch
 import torch.nn as nn
 import torch.autograd as autograd
@@ -26,17 +26,29 @@ val_x = autograd.Variable(torch.Tensor(val_x))
 
 
 class VAE(nn.Module):
-    def __init__(self, input_dim, hidden_dim):
+    def __init__(self, input_dim, hidden_dim, latent_dim):
         super(VAE, self).__init__()
         self.n_dim = hidden_dim
         self.in_dim = input_dim
+        self.latent_dim = latent_dim
         # Encoder
         self.encoder_nn1 = nn.Linear(self.in_dim, self.n_dim)
-        self.encoder_mu = nn.Linear(self.n_dim, self.n_dim)  # mean : n_dim x 1
-        self.encoder_log_sigma_square = nn.Linear(self.n_dim, self.n_dim)  # log_sigma_square : n_dim x 1
+        self.encoder_mu = nn.Linear(self.n_dim, self.latent_dim)  # mean : latent_dim x 1
+        self.encoder_logvar = nn.Linear(self.n_dim, self.latent_dim)  # logvar : latent_dim x 1
         # Decoder
-        self.decoder_nn1 = nn.Linear(self.n_dim, self.n_dim)
+        self.decoder_nn1 = nn.Linear(self.latent_dim, self.n_dim)
         self.decoder_out = nn.Linear(self.n_dim, self.in_dim)
+
+    def encode(self, x):
+        e_h = F.tanh(self.encoder_nn1(x))
+        mu = self.encoder_mu(e_h)
+        logvar = self.encoder_logvar(e_h)
+        return mu, logvar
+
+    def decode(self, z):
+        o_h = F.tanh(self.decoder_nn1(z))
+        out = F.sigmoid(self.decoder_out(o_h))
+        return out
 
     def forward(self, x):
         """
@@ -44,35 +56,27 @@ class VAE(nn.Module):
             :return x_new: batch x input_dim: The reconstructed image
         """
         # Encode
-        e_h1 = F.tanh(self.encoder_nn1(x))
-        mu = self.encoder_mu(e_h1)
-        log_sigma_square = self.encoder_log_sigma_square(e_h1)
-        sigma = torch.exp(log_sigma_square / 2)
-        random_seed = np.random.multivariate_normal(np.zeros((self.n_dim,)), np.eye(self.n_dim, self.n_dim), x.size(0))
+        mu, logvar = self.encode(x)
+        sigma = torch.exp(logvar / 2)
+        random_seed = np.random.multivariate_normal(np.zeros((sigma.size(1),)), np.eye(sigma.size(1), sigma.size(1)), x.size(0))
         z = mu + sigma * autograd.Variable(torch.Tensor(random_seed))
-
-        # Decoder
-        o_h1 = F.tanh(self.decoder_nn1(z))
-        o_out = F.sigmoid(self.decoder_out(o_h1))
-        return o_out, mu, log_sigma_square
+        # Decode
+        out = self.decode(z)
+        return out, mu, logvar
 
     def generate(self, image=None):
         """
             Generates sample using random seed if image is not given, else conditioned on the image
             :param image: 1 x input_dim: The image as generated
         """
-        random_seed = np.random.multivariate_normal(np.zeros((self.n_dim,)), np.eye(self.n_dim, self.n_dim), 1)
+        random_seed = np.random.multivariate_normal(np.zeros((self.latent_dim,)), np.eye(self.latent_dim, self.latent_dim), 1)
         random_seed = autograd.Variable(torch.Tensor(random_seed))
         if image is not None:
-            e_h1 = F.relu(self.encoder_nn1(image))
-            mu = self.encoder_mu(e_h1)
-            log_sigma_square = self.encoder_log_sigma_square(e_h1)
-            sigma = torch.exp(log_sigma_square / 2)
+            mu, logvar = self.encode(image)
+            sigma = torch.exp(logvar / 2)
             random_seed = mu + sigma * random_seed
         # Now use the random seed to generate the image
-        o_h1 = F.tanh(self.decoder_nn1(random_seed))
-        o_out = F.sigmoid(self.decoder_out(o_h1))
-        return o_out
+        return self.decode(random_seed)
 
 
 def plot_image(new_image, original_image, filename, epoch):
@@ -91,10 +95,10 @@ def plot_image(new_image, original_image, filename, epoch):
     plt.savefig(filename)
 
 
-vae = VAE(train_x.size(1), 500)
-EPOCHS = 500
+vae = VAE(train_x.size(1), 400, 20)
+EPOCHS = 100
 BATCH_SIZE = 100
-GENERATE_AFTER = 10
+GENERATE_AFTER = 5
 optimizer = torch.optim.Adagrad(vae.parameters(), lr=0.01)
 bce_loss = torch.nn.BCELoss()
 steps = 0.
@@ -106,21 +110,19 @@ for epoch in xrange(EPOCHS):
     for ii, ix in enumerate(xrange(0, train_x.size(0), BATCH_SIZE)):
         batch_x = train_x[ix: ix + BATCH_SIZE]
         optimizer.zero_grad()
-        new_x, mu, log_sigma_square = vae.forward(batch_x)
+        new_x, mu, logvar = vae.forward(batch_x)
         # Compute the loss
         loss_generative = bce_loss(new_x, batch_x)
-        # loss_kl = torch.sum(0.5 * (torch.sum((mu * mu) + torch.exp(log_sigma) - log_sigma - 1., -1)), -1) / BATCH_SIZE
-        loss_kl = torch.sum(0.5 * (torch.sum((mu * mu) + torch.exp(log_sigma_square) - log_sigma_square - 1., -1)), -1) / BATCH_SIZE
-        loss = loss_generative + loss_kl.mul_(1. - np.exp(-steps / T))
-        # loss = loss_generative + loss_kl
+        loss_kl = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        # Normalizing by the number of reconstructions seems clutch.
+        # Doesn't work without it. Am not really sure why though ...
+        loss_kl /= BATCH_SIZE * 784
+        loss = loss_generative + loss_kl
         loss.backward()
         optimizer.step()
         steps += 1.
         bar.update(ii + 1, values=[("generative_loss", loss_generative.data.numpy()[0]), ("KL_loss", loss_kl.data.numpy()[0]), ("total_loss", loss.data.numpy()[0])])
     if (epoch + 1) % GENERATE_AFTER == 0:
-        if random() < 0.5:
-            original_image = choice(val_x).view(1, -1)
-        else:
-            original_image = None
+        original_image = choice(val_x).view(1, -1)
         image = vae.generate(original_image)
         plot_image(image, original_image, "Images/Image_epoch%d.png" % (epoch + 1), epoch + 1)
